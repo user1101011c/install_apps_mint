e58cd45668eeab97d7e9e212606b61b247f56b65
user1101011c <160432639+user1101011c@users.noreply.github.com>
2025-10-28 19:28:28 +0300

    Add files via upload

---
+++ install_apps_mint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y && sudo apt install tmux libxcb-cursor0 libxcb-xinerama0 flameshot vlc curl wget build-essential git valgrind uuid-dev iasl nasm meson ninja-build neovim tor tor-socks nmap tcpdump net-tools gdb strace ltrace unzip unrar p7zip-full gnupg qemu-system

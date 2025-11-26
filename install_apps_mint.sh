fa282bfa1c552c3e72a08cdae23847ca5c93afa4
user1101011c <160432639+user1101011c@users.noreply.github.com>
2025-11-26 18:27:06 +0300

    Update install_apps_mint.sh

--- install_apps_mint.sh
+++ install_apps_mint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 
-sudo apt update && sudo apt upgrade -y && sudo apt install -y tmux libxcb-cursor0 libxcb-xinerama0 flameshot vlc curl wget build-essential git valgrind uuid-dev iasl nasm meson ninja-build neovim tor tor-socks nmap tcpdump net-tools gdb strace ltrace unzip unrar p7zip-full gnupg qemu-system pavucontrol alsamixer alsa
+sudo apt update && sudo apt upgrade -y && sudo apt install -y tmux libxcb-cursor0 libxcb-xinerama0 flameshot vlc curl wget build-essential git valgrind uuid-dev iasl nasm meson ninja-build neovim tor tor-socks nmap tcpdump net-tools gdb strace ltrace unzip unrar p7zip-full gnupg qemu-system pavucontrol alsamixer alsa gnu-efi ovmf
+
 
